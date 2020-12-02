070ef5fdf45fe47f236876a328f0c62ca231311d
Vladimir Ponomarenko <vponomaren@c1r6p1.khpi.ucode-connect.study>
2020-12-02 15:28:26 +0200

    test

--- test_CLIENT.c
+++ test_CLIENT.c
@@ -9,6 +9,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
+
 #define SERVERADDR "127.0.0.1"
 
 int main() { 
@@ -18,19 +19,38 @@ int main() {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in dest_addr;
     dest_addr.sin_family = AF_INET;
-    dest_addr.sin_port = htons(8001);
+    dest_addr.sin_port = htons(8000);
 
-    struct HOSTENT *hst;
+    //struct HOSTENT *hst;
     server = gethostbyname(SERVERADDR);
     memset((char *) &serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     memcpy(&serv_addr.sin_addr.s_addr,    server->h_addr,  server->h_length);
 
   
-    serv_addr.sin_port = htons(8001);
+    serv_addr.sin_port = htons(8000);
     connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-    //sleep(1); 
-    int nsize = recv(sock, &buff[0], sizeof(buff) - 1, 0);
-    write(1,buff, strlen(buff));
+        char str[10];// = "test";
+        scanf("%s", str);
+    while (1) {
+        sleep(3);
+        printf("YOU INPUTED %s\n", str);
+        send(sock, str, sizeof(str), 0);
+
+        int nsize = recv(sock, &buff[0], sizeof(buff) - 1, 0);
+        if (nsize < 0) {
+            continue;
+        }
+        else
+        {
+            printf("Server has returt %s\n", &buff[0]);
+            //write(1,buff, strlen(buff));
+        }
+              
+
+
+    }
+
+    
     close(sock);
 }
--- test_SERVER.c
+++ test_SERVER.c
@@ -7,14 +7,39 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdbool.h>
+#include <pthread.h>
 #define BUF_SIZE 256;
+
+void *func(void* sock) {
+        char buff[256];
+        char buff2[256];
+           printf("CONNECTED\n");
+           int *temp = sock;
+           int client_socket = *temp;
+            while (1) {
+            int nsize = recv(client_socket, &buff2[0], sizeof(buff2) - 1, 0);
+            if (nsize == 0)
+                break;
+            printf("%d\n", nsize);
+            if (nsize < 0)
+                continue;
+
+            else {
+                //printf ("Server took : %s\n", buff2);
+                send(client_socket, &buff2[0], sizeof(&buff2[0]) - 1, 0);
+            }
+        }
+       // printf("CONNECTION CLOSED\n");
+        close(client_socket);
+        return NULL;
+}
 
 int main() {
     // Инициализируем сокет!
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         write(2, "SOCK() ERROR\n", 13);
-        exit(-1);
+        return -1;
     }
     write(1, "SOCK SUCCESS\n",13);
 
@@ -22,7 +47,7 @@ int main() {
     //Вводим информацию для связывания
     struct sockaddr_in sock_info;
     sock_info.sin_family = AF_INET;
-    sock_info.sin_port = htons(8001); // переводим порт в сетевой порядок
+    sock_info.sin_port = htons(8000); // переводим порт в сетевой порядок
     sock_info.sin_addr.s_addr = 0;    // сервер принимает подключения на все свои IP-адреса
 
 
@@ -42,13 +67,41 @@ int main() {
     write(1, "LISTEN SUCCESS\n Waiting connection...\n",38);
 
     // Получаем сообщения из сокета
+    int ph_count;
+    pthread_t pthreads[5];
     int client_socket; // сокет для клиента
     struct sockaddr_in client_addr; // адрес клиента (заполняется системой)
     int client_addr_size = sizeof(client_addr);
     while ((client_socket = accept(sock, (struct sockaddr *)&client_addr, &client_addr_size))) {
         write(1,"CONNETCTED: \n",13);
-        send(client_socket, "Hello", sizeof("Hello"), 0);
+        //struct DWORD thID;
+
+        pthread_create(&pthreads[ph_count], NULL, func, &client_socket);
+      //  pthread_join(pthreads[ph_count], NULL);
+        ph_count++;
+       // pthread_create(&pthreads[0], NULL, test_func, NULL);
+
+        write(1,"PTHREAD CREATED: \n",18);
+
+        //send(client_socket, "Hello", sizeof("Hello"), 0);
+        /*
+        while (1) {
+            int nsize = recv(client_socket, &buff2[0], sizeof(buff2) - 1, 0);
+            if (nsize == 0)
+                break;
+            printf("%d\n", nsize);
+            if (nsize < 0)
+                continue;
+
+            else {
+                printf ("Server took : %s\n", buff2);
+                send(client_socket, &buff2[0], sizeof(buff2), 0);
+            }
+        }
+    
+        printf("CONNECTION CLOSED\n");
         close(client_socket);
+            */
     }
     close(sock);
     return 0;
