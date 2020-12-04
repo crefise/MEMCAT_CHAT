31396fc373af49ca42666da9d9262a516ee7486a
Vladimir <crefise@gmail.com>
2020-12-04 02:23:34 +0200

    hello

--- test_SERVER.c
+++ test_SERVER.c
@@ -9,28 +9,66 @@
 #include <stdbool.h>
 #include <pthread.h>
 #define BUF_SIZE 256;
-
-void *func(void* sock) {
-        char buff[256];
-        char buff2[256];
-           printf("CONNECTED\n");
+int ph_count = 0; // количество подключенных клиентов
+// massage[id][id].....
+// dialogs[id]
+// text_massages[id]
+int parse_solution(char *text) {
+    if (text[0] == 'm')
+        return 1;
+    if (text[0] == 'd')
+        return 2;
+    if (text[0] == 't')
+        return 3;
+    return -1;
+}
+void *user_connect(void* sock) {
+           char buff[256]; // client - massage
            int *temp = sock;
            int client_socket = *temp;
-            while (1) {
-            int nsize = recv(client_socket, &buff2[0], sizeof(buff2) - 1, 0);
-            if (nsize == 0)
+           bool exit = 0;
+            while (!exit) {
+            int nsize = recv(client_socket, &buff[0], sizeof(buff) - 1, 0);
+            if (nsize == 0) {
+                write(2, "CLIENT CLOSE CONNECTION\n", 24);
                 break;
+            }
             printf("%d\n", nsize);
             if (nsize < 0)
                 continue;
-
             else {
-                //printf ("Server took : %s\n", buff2);
-                send(client_socket, &buff2[0], sizeof(&buff2[0]) - 1, 0);
+                pthread_t pthread;
+                printf ("Server tooked : %s\n", buff);
+                int solution = parse_solution(buff);
+                switch (solution)
+                {
+                case 1: // Хотим написать сообщение
+                    write(2, "ADD MASSAGE\n",13);
+                    exit = 1;
+                    break;
+                case 2: // Хотим обновить диалоги
+                    write(2, "UPPDATE DIALOGS\n",16);
+                    exit = 1;
+                    break;
+                case 3: // Хотим обновить сообщения в диалоге
+                    write(2, "UPPDATE TEXT IN DIALOG\n",23);
+                    exit = 1;
+                    break;
+                case -1: // ошибка сообщения
+                    write(2, "-1 ERROR\n",9);
+                    exit = 1;
+                    break;
+                default: // неизвесная ошибка
+                    write(2, "UKNOWN ERROR\n",13);
+                    exit = 1;
+                    break;
+                }
+
             }
         }
-       // printf("CONNECTION CLOSED\n");
+        printf("CONNECTION  CLOSED\n");
         close(client_socket);
+        ph_count--;
         return NULL;
 }
 
@@ -55,53 +93,28 @@ int main() {
     if (bind(sock, (struct sockaddr *)&sock_info, sizeof(sock_info))) {
         write(2, "BIND() ERROR\n", 13);
         close(sock); // закрываем сокетб перед выходом
-        exit(-1);
+        return -1;
     }
     write(1, "BIND SUCCESS\n",13);
 
     //Устанавливаем очередь для сокета & Ожидаем подключения
     if (listen(sock, 0x100) < 0) {
         write(2, "LISTEN() ERROR\n", 15);
-        exit(-1);
+        return -1;
     }
     write(1, "LISTEN SUCCESS\n Waiting connection...\n",38);
 
     // Получаем сообщения из сокета
-    int ph_count;
     pthread_t pthreads[5];
     int client_socket; // сокет для клиента
     struct sockaddr_in client_addr; // адрес клиента (заполняется системой)
-    int client_addr_size = sizeof(client_addr);
+    unsigned int client_addr_size = (unsigned int)sizeof(client_addr);
     while ((client_socket = accept(sock, (struct sockaddr *)&client_addr, &client_addr_size))) {
         write(1,"CONNETCTED: \n",13);
-        //struct DWORD thID;
-
-        pthread_create(&pthreads[ph_count], NULL, func, &client_socket);
-      //  pthread_join(pthreads[ph_count], NULL);
+        pthread_create(&pthreads[ph_count], NULL, user_connect, &client_socket);
         ph_count++;
-       // pthread_create(&pthreads[0], NULL, test_func, NULL);
-
+        printf("Подключенные клиенты - %d\n",  ph_count);
         write(1,"PTHREAD CREATED: \n",18);
-
-        //send(client_socket, "Hello", sizeof("Hello"), 0);
-        /*
-        while (1) {
-            int nsize = recv(client_socket, &buff2[0], sizeof(buff2) - 1, 0);
-            if (nsize == 0)
-                break;
-            printf("%d\n", nsize);
-            if (nsize < 0)
-                continue;
-
-            else {
-                printf ("Server took : %s\n", buff2);
-                send(client_socket, &buff2[0], sizeof(buff2), 0);
-            }
-        }
-    
-        printf("CONNECTION CLOSED\n");
-        close(client_socket);
-            */
     }
     close(sock);
     return 0;
