3a1bcdcedffd83b38c9f1df526b1285ba592da03
Serhii Vlokh <svlokh@c1r6p2.khpi.ucode-connect.study>
2020-12-09 13:08:54 +0200

    dfd

--- Server/src/parsing.c
+++ Server/src/parsing.c
@@ -1,9 +1,29 @@
 #include "header.h"
 
+static char* add_char_to_str(char* str, char ch) {
+    int len1 = strlen(str);
+    int len2 = 1;                      
+    char *result = malloc(len1 + len2 + 1);
+    if (!result) {
+        fprintf(stderr, "malloc() failed: insufficient memory!\n");
+        return NULL;
+    }
+    memcpy(result, s1, len1);
+    memcpy(result + len1, s2, len2 + 1);    
+    return result;
+}
+
 char** ps_registration(char* src) {
     
 }
 
 char** ps_login(char* src) {
-    
+    int index = 6;
+    int login_length, pass_length;
+    char* login_length_s;
+    char* pass_length_s;
+    char** result = (char*)malloc(sizeof(char*) * 2);
+    //result[0] = (char)malloc(sizeof(char))
+
+    return result;
 }
