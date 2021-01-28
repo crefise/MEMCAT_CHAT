02c3150bc2d9b6f464022523425ab72660751e59
vipidlatiu <vipidlatiu@stud.khpi.ucode-connect.study>
2021-01-28 14:26:40 +0200

    message 0.1

--- gtk_learn/gtk.c
+++ gtk_learn/gtk.c
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include <string.h>
 
 void load_css ( void ) {
     GtkCssProvider *provider;
@@ -18,26 +19,39 @@ void load_css ( void ) {
     /// ***
 }
 
-void my_close_app(GtkWidget *window, gpointer data)
-{
-    gtk_main_quit();
-}
+struct message_struct {
+    GtkWidget *input_str;
+    GtkWidget *message_list_box;
+};
+
 #define SIZE 4
 #define SIZE_C 5
 #include <malloc/malloc.h>
 
+void send(GtkWidget *button, gpointer input)
+{   
+    struct message_struct *temp = input;
+
+    char *text = (char*)gtk_entry_get_text(GTK_ENTRY(temp->input_str));
+    if(text) {
+        GtkWidget *message = gtk_label_new(text);
+        gtk_widget_set_name(message, "message");
+        gtk_box_pack_start(GTK_BOX(temp->message_list_box), message, FALSE, FALSE, 5);
+        gtk_widget_show_all(temp->message_list_box);
+    }
+}
 
 int main(int argc, char *argv[]) {
     GtkWidget *window; // my window
-    GtkWidget *main_box,*chats_list_box,  *message_list_box, *message_box, *input_box, *search_box, *chats_box; // Боксы 
-    GtkWidget *input_str, *input_key; // for imput_box
+    GtkWidget *main_box,*chats_list_box, *message_box, *input_box, *search_box, *chats_box; // Боксы 
+    GtkWidget *input_key; // for imput_box
     GtkWidget *search_str, *search_key;
     char *messages[] = {"Hello", "mello", "tello", "pello"};
     char *chat_list[] = {"Kostya", "Vova", "Sergii", "Viktor", "Vlad"};
     GtkWidget *messages_label[SIZE], *chat_list_label[SIZE_C];
     GtkWidget *scrool_massages, *scrool_chats;
 
-
+    struct message_struct *MessageInfo =  malloc(sizeof(struct message_struct));
     
  
     gtk_init(&argc, &argv);
@@ -48,13 +62,13 @@ int main(int argc, char *argv[]) {
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_window_set_default_size(GTK_WINDOW(window), 600, 600);
 
-    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(my_close_app), NULL); // Сигнал для завершения преложения
+    g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL); // Сигнал для завершения преложения
 
 
 
 // CREATE NEW THING
     input_key = gtk_button_new_with_label("SEND");
-    input_str = gtk_entry_new();
+    MessageInfo->input_str = gtk_entry_new();
 
     search_key = gtk_button_new_with_label("SEARCH");
     search_str = gtk_entry_new();
@@ -63,7 +77,7 @@ int main(int argc, char *argv[]) {
     chats_list_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     message_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     input_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-    message_list_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+    MessageInfo->message_list_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     search_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
     chats_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
 
@@ -95,7 +109,7 @@ int main(int argc, char *argv[]) {
     gtk_box_pack_start(GTK_BOX(message_box), scrool_massages, TRUE, TRUE, 0);
     gtk_box_pack_start(GTK_BOX(message_box), input_box, FALSE, FALSE, 0);
 
-    gtk_box_pack_start(GTK_BOX(input_box), input_str, TRUE, TRUE, 2);
+    gtk_box_pack_start(GTK_BOX(input_box), MessageInfo->input_str, TRUE, TRUE, 2);
     gtk_box_pack_start(GTK_BOX(input_box), input_key, FALSE, FALSE, 2);
 
     gtk_box_pack_start(GTK_BOX(chats_box), search_box, FALSE, FALSE, 0);
@@ -105,17 +119,18 @@ int main(int argc, char *argv[]) {
     gtk_box_pack_start(GTK_BOX(search_box), search_key, FALSE, FALSE, 0);
 
     for (int i = 0; i < SIZE; i++) // PACK MESSAGES
-        gtk_box_pack_start(GTK_BOX(message_list_box), messages_label[i], FALSE, FALSE, 5);
+        gtk_box_pack_start(GTK_BOX(MessageInfo->message_list_box), messages_label[i], FALSE, FALSE, 5);
     for (int i = 0; i < SIZE_C; i++) // PACK CHAT_LIST
         gtk_box_pack_start(GTK_BOX(chats_list_box), chat_list_label[i], FALSE, FALSE, 1);
 // END PACK
 
 // CONTAIN ALL
-    gtk_container_add(GTK_CONTAINER(scrool_massages), message_list_box); 
+    gtk_container_add(GTK_CONTAINER(scrool_massages), MessageInfo->message_list_box); 
     gtk_container_add(GTK_CONTAINER(scrool_chats), chats_list_box);
     gtk_container_add(GTK_CONTAINER(window), main_box);
 // CONTAIN ALL END
 
+    g_signal_connect(input_key, "clicked", G_CALLBACK (send), MessageInfo);
 
     gtk_widget_show_all(window);
 
