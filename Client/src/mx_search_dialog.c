82244f714c8779f6e6fcc5e595eea34fb2b7ac99
crefise <crefise@gmail.com>
2021-02-11 17:01:32 +0200

    TEsting

--- Client/src/mx_search_dialog.c
+++ Client/src/mx_search_dialog.c
@@ -28,6 +28,16 @@ void mx_search_dialog(GtkWidget *button, gpointer data) {
         g_signal_connect(G_OBJECT(mx_find_name_chat(MY_CHATS, text)->chat_button), "clicked", G_CALLBACK(select_chat), (gpointer)mx_find_name_chat(MY_CHATS, text));
         gtk_widget_show_all(window);
         gtk_widget_hide(reconnect_widget);
+
+        for (int i = 0; mx_get_index_chat(MY_CHATS, i)  && mx_get_index_chat(MY_CHATS, i) != NULL; i++) {
+            gtk_widget_hide(mx_get_index_chat(MY_CHATS, i)->message_list_box);
+        }  
+        if (strcmp(FAVORITE_CHAT_DEFINE, OPENED_DIALOG) == 0) {
+            gtk_widget_show_all(FAVORITE_CHAT->message_list_box);
+        } 
+        else {
+            gtk_widget_show_all(mx_find_name_chat(MY_CHATS, OPENED_DIALOG)->message_list_box);
+        }
     } 
     else {
         mx_printerr("ERROR CREATING DIALOG");
